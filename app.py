481d4e70ab5da743d01398847f2bf79918f9b19e
Jessica Garson <jgarson@twitter.com>
2022-09-29 11:39:46 -0400

    Update app.py
    Signed-off-by: Jessica Garson <jgarson@twitter.com>

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
+from dash import dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
